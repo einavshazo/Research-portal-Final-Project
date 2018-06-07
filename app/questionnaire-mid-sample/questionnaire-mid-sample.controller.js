c9020f3b839eec3f04a5eb91856497e48566207f
Einav <einavshazo@gmail.com>
2018-06-07 17:47:27 +0300

    Realizing functionality for questionnaire-mid-sample screens and saving data in DB

--- app/questionnaire-mid-sample/questionnaire-mid-sample.controller.js
+++ app/questionnaire-mid-sample/questionnaire-mid-sample.controller.js
@@ -3,6 +3,9 @@ angular.module('orledor')
 		
 		var userName = $stateParams.userName;
 		var researchName = $stateParams.researchName;
+		
+		var clientDate = new Date();		
+		var  date  = new Date(clientDate).toDateString("dd-MM-yyyy");
 
 		firebase.child('users').child(userName).once('value')
         .then(function(user) {
@@ -10,17 +13,18 @@ angular.module('orledor')
         })
         .then(function() {
                 $scope.$apply();
-		});
-		
+        });
+
 
 		firebase.child('researches').child(researchName).once('value')
         .then(function(research) {
 			$scope.researchName = research.child("_researchName").val();
+			$scope.researchNumber = research.child("_researchNumber").val();
         })
         .then(function() {
                 $scope.$apply();
-        });
-
+		});
+		
 
 //------------------------------------------------------------------------------
 		grade
@@ -36,8 +40,8 @@ angular.module('orledor')
 
 
 
-		$scope.questionnaire_mid_sample = function (ev) {
-			return ensureQuestionnaire4()
+		$scope.questionnaireMidSample = function (ev) {
+			return ensurequestionnaireMidSample()
 				.catch(function (err) {
 					$mdDialog.show(
 						$mdDialog.alert()
@@ -49,43 +53,76 @@ angular.module('orledor')
 					return $q.reject();
 				})
 				.then(function () {
-					// $scope.user._birthDate = $scope.selectedBirthDate.toISOString();
-
-					return firebase.child('users').child($scope.user._userName).set($scope.user);										
+										
+					return firebase.child('users').child(userName).child("userResearches")
+					.child($scope.researchName).child("questionnaireMidSample").child(date).set({
+						_love : 				$scope.questionnaireMidSample._love,
+						_media: 				$scope.questionnaireMidSample._media,
+						_continue: 				$scope.questionnaireMidSample._continue,
+						_interest_in_others_txt:$scope.questionnaireMidSample._interest_in_others_txt,
+						_other_media: 			$scope.questionnaireMidSample._other_media,
+						another_question_1_txt: $scope.questionnaireMidSample.another_question_1_txt,
+						another_question_1_num: $scope.questionnaireMidSample.another_question_1_num,
+						another_question_2_txt: $scope.questionnaireMidSample.another_question_2_txt,
+						another_question_2_num:	$scope.questionnaireMidSample.another_question_2_num,
+						
+						_introvert_num:			$scope.questionnaireMidSample._introvert_num,
+						_curious_num:			$scope.questionnaireMidSample._curious_num,
+						_enthusiastic_num:		$scope.questionnaireMidSample._enthusiastic_num,
+						_talker_num:			$scope.questionnaireMidSample._talker_num,
+						_tired_num:				$scope.questionnaireMidSample._tired_num,
+						_apathetic_num:			$scope.questionnaireMidSample._apathetic_num,
+						_personal_life_num:		$scope.questionnaireMidSample._personal_life_num,
+						_interest_in_others_num:$scope.questionnaireMidSample._interest_in_others_num,
+						
+						_watched_media_txt:		$scope.questionnaireMidSample._watched_media_txt,
+						_general_impression:	$scope.questionnaireMidSample._general_impression,
+						_emotional_response_txt:$scope.questionnaireMidSample._emotional_response_txt,
+						_physical_reaction_txt:	$scope.questionnaireMidSample._physical_reaction_txt,
+						_physical_change_txt:	$scope.questionnaireMidSample._physical_change_txt,
+						_emotional_change_txt:	$scope.questionnaireMidSample._emotional_change_txt,
+						
+						_watched_media_num:		$scope.questionnaireMidSample._watched_media_num,
+						_other_detail_txt:		$scope.questionnaireMidSample._other_detail_txt,
+						
+						_emotional_response_num:$scope.questionnaireMidSample._emotional_response_num,
+						_physical_reaction_num:	$scope.questionnaireMidSample._physical_reaction_num,
+						_physical_change_num:	$scope.questionnaireMidSample._physical_change_num,
+						_emotional_change_num:	$scope.questionnaireMidSample._emotional_change_num
+					});									
 				})
 				.then(function () {
-					loggedUser.setUser($scope.user);
-					$state.go('home');
+					$state.go('selected-research', {'name': $scope.researchName, 'id': $scope.researchNumber});
 				})
 				.catch(function (err) {
 					console.log(err);
 				});
 		}
 
-		function ensureQuestionnaire4() {
-			if (!$scope.user._userName) {
-				return $q.reject('חובה למלא שם משתמש')
-			}
+		function ensurequestionnaireMidSample() {
+			// if (!$scope.user._userName) {
+			// 	return $q.reject('חובה למלא שם משתמש')
+			// }
 
-			if (!$scope.user._password) {
-				return $q.reject('חובה למלא סיסמא')
-			}
+			// if (!$scope.user._password) {
+			// 	return $q.reject('חובה למלא סיסמא')
+			// }
 
-			if ($scope.user._password !== $scope.retypePassword) {
-				return $q.reject('הסיסמא והוידאוי סיסמא אינם תואמים')
-			}
+			// if ($scope.user._password !== $scope.retypePassword) {
+			// 	return $q.reject('הסיסמא והוידאוי סיסמא אינם תואמים')
+			// }
 
-			if (!$scope.user._firstName) {
-				return $q.reject('חובה למלא שם מלא')
-			}
+			// if (!$scope.user._firstName) {
+			// 	return $q.reject('חובה למלא שם מלא')
+			// }
 
-			if (!$scope.selectedBirthDate) {
-				return $q.reject('חובה למלא תאריך לידה')
-			}
+			// if (!$scope.selectedBirthDate) {
+			// 	return $q.reject('חובה למלא תאריך לידה')
+			// }
 
-			if (!$scope.user._language || !$scope.user._language.length) {
-				return $q.reject('חובה למלא לפחות שפה מדוברת אחת')
-			}
+			// if (!$scope.user._language || !$scope.user._language.length) {
+			// 	return $q.reject('חובה למלא לפחות שפה מדוברת אחת')
+			// }
 
 			return $q.resolve();
 		}
