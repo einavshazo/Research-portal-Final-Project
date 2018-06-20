704240ddc622c4c3cd57523c45ca3eb514a76b38
Einav <einavshazo@gmail.com>
2018-06-20 12:27:52 +0300

    Small changes

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ angular.module('orledor', ['ui.router', 'ngMaterial', 'chart.js']);
 angular.module('orledor')
 	.config(function($urlRouterProvider)
     {
-			var port = process.env.PORT || 8082;
 		$urlRouterProvider.when('', '/login');
 		$urlRouterProvider.when('/', '/login');    	
     });
