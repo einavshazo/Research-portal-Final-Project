1ca0ab5199b64ccffcbde7fcda2b94eb8f0cd794
Einav <einavshazo@gmail.com>
2018-06-20 13:13:43 +0300

    Small changes #2

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-angular.module('orledor', ['ui.router', 'ngMaterial', 'chart.js']);
 
+// var app = angular.module('app.controllers', []);
+
+// app.controller('CalculatorCtrl', ['$scope', '$http', 'CalculatorService', function($scope, $http, CalculatorService){
+
+angular.module('orledor')
+	.controller('researcherController', function($scope, $http) {
+
+
+
+// angular.module('orledor', ['ui.router', 'ngMaterial', 'chart.js']);
+
+angular.controller()
 angular.module('orledor')
 	.config(function($urlRouterProvider)
     {
 		$urlRouterProvider.when('', '/login');
 		$urlRouterProvider.when('/', '/login');    	
-    });
+		});
+	});
