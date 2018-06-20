2e7eaae1fc7a40af5d0e88e71c44dfe25bbfe3a9
Einav <einavshazo@gmail.com>
2018-06-20 11:32:27 +0300

    add port for app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ angular.module('orledor', ['ui.router', 'ngMaterial', 'chart.js']);
 angular.module('orledor')
 	.config(function($urlRouterProvider)
     {
+			var port = process.env.PORT || 8082;
 		$urlRouterProvider.when('', '/login');
 		$urlRouterProvider.when('/', '/login');    	
     });
