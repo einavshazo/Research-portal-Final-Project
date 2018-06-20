59f555f86b0de6f0fbd228d2cec63b0c480169e6
Einav <einavshazo@gmail.com>
2018-06-20 18:46:21 +0300

    restore to last version

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 
-angular.module('orledor')
-	.controller('researcherController', function($scope, $http) {
+angular.module('orledor', ['ui.router', 'ngMaterial', 'chart.js']);
 
-// angular.module('orledor', ['ui.router', 'ngMaterial', 'chart.js']);
-
-angular.controller()
 angular.module('orledor')
 	.config(function($urlRouterProvider)
     {
 		$urlRouterProvider.when('', '/login');
 		$urlRouterProvider.when('/', '/login');    	
 		});
-	});
+
